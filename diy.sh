065263c503eb6597d8c8dd53b089766fd19c735c
honghushui123 <60507588+honghushui123@users.noreply.github.com>
2022-05-25 12:29:49 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -30,10 +30,10 @@ echo '修改banner'
 rm -rf package/base-files/files/etc/banner
 cp -f ../banner package/base-files/files/etc/
 
-echo '下载ServerChan'
-git clone https://github.com/tty228/luci-app-serverchan ../diy/luci-app-serverchan
+#echo '下载ServerChan'
+#git clone https://github.com/tty228/luci-app-serverchan ../diy/luci-app-serverchan
 
-echo '下载AdGuard Home'
+#echo '下载AdGuard Home'
 #svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-adguardhome ../diy/luci-app-adguardhome 
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome ../diy/luci-app-adguardhome
 svn co https://github.com/kenzok8/openwrt-packages/trunk/adguardhome ../diy/adguardhome
